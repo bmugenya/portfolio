57891698eb5d5ba97e670732a67d7df10aac6605
Bmugenya <bmugenya26@gmail.com>
2021-02-16 23:38:01 +0300

    added skills

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,31 +1,139 @@
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Icon, Paper, Typography } from '@material-ui/core'
 import React from 'react'
-import '../assets/css/Resume.css'
+import CustomTimeline, { CustomTimelineSeparator } from '../components/Timeline'
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
 import data from '../utils/data'
+import TimelineContent from '@material-ui/lab/TimelineContent'
+import TimelineItem from '@material-ui/lab/TimelineItem'
+import SchoolIcon from '@material-ui/icons/School'
+import TimelineDot from '@material-ui/lab/TimelineDot'
 const Resume = () => {
   return (
     <>
-      <Grid container className='section pb_45'>
+      <Grid container className='section pb_45 pt_45'>
         <Grid item className='section_title mb_30'>
           <span></span>
           <h6 className='section_text'>About Me</h6>
         </Grid>
+
         <Grid item xs={12}>
           <Typography variant='body2' className='about_text'>
             {data.about}
           </Typography>
         </Grid>
       </Grid>
-      <Grid container className='section'>
+      <Grid container className='section pb_45'>
         <Grid item className='section_title mb_30'>
           <span></span>
           <h6 className='section_text'>Resume</h6>
         </Grid>
 
         <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={12} md={6}></Grid>
-            <Grid item xs={12} md={6}></Grid>
+          <Grid container className='resume_timeline'>
+            <Grid item xs={12} md={6}>
+              <CustomTimeline
+                title='Certifications'
+                icon={<VerifiedUserIcon />}
+              >
+                {data.certifications.map((certification) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent class_name='timeline_content'>
+                      <Typography className='timeline_title'>
+                        {certification.title}
+                      </Typography>
+                      <Typography variant='captain' className='timeline_date'>
+                        {certification.date}
+                      </Typography>
+                      <Typography
+                        variant='body2'
+                        className='timeline_description'
+                      >
+                        {certification.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <CustomTimeline title='Education' icon={<SchoolIcon />}>
+                {data.education.map((school) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent class_name='timeline_content'>
+                      <Typography className='timeline_title'>
+                        {school.title}
+                      </Typography>
+                      <Typography variant='captain' className='timeline_date'>
+                        {school.date}
+                      </Typography>
+                      <Typography
+                        variant='body2'
+                        className='timeline_description'
+                      >
+                        {school.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container className='section pb_45'>
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h5 className='section_title'>My services</h5>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container spacing={3} justify={'space-around'}>
+            {data.services.map((service) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <div className='service'>
+                  <Icon className='service_icon'>{service.icon}</Icon>
+                  <Typography variant='h6' className='service_title'>
+                    {service.title}
+                  </Typography>
+                  <Typography variant='body' className='service_description'>
+                    {service.description}
+                  </Typography>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+
+          <Grid container className='section graybg pt_45  p_50'>
+            <Grid item xs={12}>
+              <Grid container justify='space-between' spacing={3}>
+                {data.skills.map((skill) => (
+                  <Grid item xs={12} sm={6} md={3}>
+                    <Paper elevation={0} class Name='skill'>
+                      <Typography variant='h6' className='skill_title'>
+                        {skill.title}
+                      </Typography>
+
+                      {skill.description.map((element) => (
+                        <Typography
+                          variant='body2'
+                          className='skill_description'
+                        >
+                          <TimelineDot
+                            variant={'outlined'}
+                            className='timeline_dot'
+                          />
+                          {element}
+                        </Typography>
+                      ))}
+                    </Paper>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,6 +3,7 @@ import FacebookIcon from '@material-ui/icons/Facebook'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import GitHubIcon from '@material-ui/icons/GitHub'
+import { WebOutlined } from '@material-ui/icons'
 export default {
   name: 'Brian Mugenya',
   title: 'Full stack developer',
@@ -33,4 +34,79 @@ export default {
   },
   about:
     'I have a proficiency of bring others together to resolve conflicts and reconciling differences through negotiation. I am calm and collected even under stressful situations .I am strongly passionate about innovative ideas and problem-solving using programming technologies.\n\nI am a time conscious, self-driven Individual who is eager to learn and apply new skills and knowledge to the work place. My open minded nature allows me to work in a multi-cultural team and adapt to new cultures.',
+
+  certifications: [
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+  ],
+  education: [
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+    {
+      title: 'work',
+      date: 'date',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+    },
+  ],
+  services: [
+    {
+      title: 'work',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+      icon: <WebOutlined />,
+    },
+    {
+      title: 'work',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+      icon: <WebOutlined />,
+    },
+    {
+      title: 'work',
+      description:
+        'together to resolve conflicts and reconciling differences through negotiation',
+      icon: <WebOutlined />,
+    },
+  ],
+  skills: [
+    {
+      title: 'FRONT-END',
+      description: ['ReactJs', 'Javascript', 'Bootsrap', 'Material UI'],
+    },
+    {
+      title: 'BACK-END',
+      description: ['Flask', 'ExpressJs', 'Javascript', 'php'],
+    },
+    {
+      title: 'FRONT-END',
+      description: ['ReactJs', 'Javascript', 'Bootsrap', 'Material UI'],
+    },
+  ],
 }
